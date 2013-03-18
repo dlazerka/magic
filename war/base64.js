1d4efdff2ea24737538eb6353e7d27b9a160d41a
Dzmitry Lazerka <dlazerka@gmail.com>
2013-03-18 11:22:22 -0700

    Added favicon. Added uploading/downloading base64 files. Some binary files still encoded incorrectly.

--- war/base64.js
+++ war/base64.js
@@ -42,31 +42,66 @@ $(function() {
 		var r = [];
 		for (var i = 0; i < s.length; i += 4) {
 			r.push(at(s, i) << 2 | at(s, i + 1) >> 4);
-			r.push((at(s, i + 1) & 15) << 4 | at(s, i + 2) >> 2);
-			r.push((at(s, i + 2) & 3) << 6 | at(s, i + 3));
+			if (s.charAt(i + 2) != '=') {
+                r.push((at(s, i + 1) & 15) << 4 | at(s, i + 2) >> 2);
+			}
+			if (s.charAt(i + 3) != '=') {
+                r.push((at(s, i + 2) & 3) << 6 | at(s, i + 3));
+			}
 		}
 		r = r.map(function(i) {return String.fromCharCode(i)});
 		return r.join('');
 	}
 
-	function encodeEl() {
-		var decoded = el('decoded').val();
-		var encoded = encode(decoded);
-		el('encoded').val(encoded);
+	function test() {
+	    //function fail() {throw 'failed'};
+	    encode('asd') === 'YXNk' || fail();
+	    decode('YXNk') === 'asd' || fail();
+	    encode('1023e') === 'MTAyM2U=' || fail();
+	    decode('MTAyM2U=') === '1023e' || fail();
+	    console.log('passed');
 	}
 
-	function decodeEl() {
-		var encoded = el('encoded').val();
-		var decoded = decode(encoded);
-		el('decoded').val(decoded);
-	}
-	
+    var blob;
+    function process(func, input) {
+        blob = func(input);
+        el('text').val(blob);
+    }
+
+    function getAndProcess(func) {
+        var fileEl = el('file')[0];
+        var file = fileEl.files[0];
+        var input;
+        if (file) {
+            var fr = new FileReader();
+            fr.onload = function() {
+                fileEl.value = '';
+                process(func, fr.result);
+            };
+            fr.readAsBinaryString(file);
+        } else {
+            input = el('text').val();
+            process(func, input);
+        }
+    }
+
+    function download(event) {
+        event.preventDefault();
+        if (!blob) {
+            test();
+            return;
+            // blob = el('text').val();
+        }
+        var uriContent = "data:application/octet-stream," + encodeURIComponent(blob);
+        window.open(uriContent);
+    }
+
 	var sample = 'Man is distinguished, not only by his reason, but by this singular passion ' +
 		'from other animals, which is a lust of the mind, that by a perseverance ' +
 		'of delight in the continued and indefatigable generation of knowledge, ' +
 		'exceeds the short vehemence of any carnal pleasure.';
-	el('decoded').val(sample);
-	encodeEl();
-	el('decoded').keyup(encodeEl);
-	el('encoded').keyup(decodeEl);
+	el('text').val(sample);
+	el('encode').on('click', function() {getAndProcess(encode)});
+	el('decode').on('click', function() {getAndProcess(decode)});
+	el('download').on('click', download);
 });
